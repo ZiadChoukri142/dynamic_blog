6b4ad8110e7915933f78ffc4196f9d2eb6e4c64e
ZiadChoukri142 <ziad.choukrii@gmail.com>
2021-04-25 09:56:51 +0000

    deleted: dashboard access

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,7 +4,7 @@
 			</div>
 			<ul>
 			  <li><a class="active" href="index.php">Home</a></li>
-			  <li><a href="./admin/dashboard.php">Dashboard</a></li>
+			  <li><a href="#news">News</a></li>
 			  <li><a href="#contact">Contact</a></li>
 			  <li><a href="#about">About</a></li>
 			</ul>
